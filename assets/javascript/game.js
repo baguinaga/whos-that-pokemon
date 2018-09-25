1e9142d84af876e087f9e9a658aacb6c5409762f
baguinaga <42448094+Baguinaga@users.noreply.github.com>
2018-09-25 00:30:51 -0400

    Remade randomOptions function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,90 @@
-var pokeImg = ["Pikachu", "Charmander", "Bulabasaur", "Squirtle", "Snorlax", "Gyarydos", "Scyther", "Mewtwo", "Gengar", "Kabutops", "Dragonair", "Ninetails", "Slowpoke", "Meowth", "Lapras"];
+// $(document).ready(function () {
+
+// global variables
+
+var pokeArray = ["Pikachu", "Charmander", "Bulabasaur", "Squirtle", "Snorlax", "Gyarydos", "Scyther", "Mewtwo", "Gengar", "Kabutops", "Dragonair", "Ninetails", "Slowpoke", "Meowth", "Lapras"];
 
 var pokeAnswer = [];
+
+var pokeObject = {};
+
 var timerCount = 0;
+
 var timerActive = false;
-var counter = 0;
 
-function randomNumExclude(array, arr) {
-  numberArray = [arr];
-  while (numberArray.length < 4) {
-    const randomNum = Math.floor(Math.random() * array.length);
-    (numberArray.includes(randomNum)) ? randomNumExclude(array, arr): numberArray.push(randomNum);
-  }
-  for (let i = numberArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [numberArray[i], numberArray[j]] = [numberArray[j], numberArray[i]];
-  }
-  return numberArray;
-}
+//functions
 
-for (var i = 0; i < pokeImg.length; i++) {
-  pokeAnswer.push(randomNumExclude(pokeImg, i));
-}
-console.log(pokeAnswer);
+function randomOptions(array, key) {
+  for (let i = 0; i < array.length; i++) {
 
+    //making key an empty array that includes the ith number in the first position
 
-function showOptions(array, key) {
+    key[i] = [];
+    key[i][0] = i
 
-    for (let i = 0; i < array.length; i++) {
-      $("#pokemonImg").attr("src", "assets/images/" + String(i) + ".png");
-      var radioDiv = $("<div>")
-      radioDiv.addClass("form-check form-check-inline");
-      $("#form").empty();
+    //function for creating 4 ( 3 + correct option) non-repeating numbers and then calling on it
 
-      for (let j = 0; j < 4; j++) {
-
-        var radioInput = $("<input>");
-        radioInput.attr({
-
-          class: "form-check-input",
-          type: "radio",
-          name: array[i],
-          value: array[key[i][j]]
-
-        })
-
-        var radioLabel = $("<label>");
-        radioLabel
-          .addClass("form-check-label")
-          .text(array[key[i][j]]);
-        radioDiv.append(radioInput, radioLabel);
-        $("#form").append(radioDiv);
-
-        //testing to see if the text and images are actually being iterated
-        console.log(array[key[i][j]]);
+    function randomNumber(array, key) {
+      while (key[i].length < 4) {
+        const randomNum = Math.floor(Math.random() * array.length);
+        (key[i].includes(randomNum)) ? randomNumber(array, key): key[i].push(randomNum);
       }
     }
+
+    randomNumber(array, key);
+
+    //shuffling the 4 options
+
+    for (let n = key[i].length - 1; n > 0; n--) {
+      const j = Math.floor(Math.random() * (n + 1));
+      [key[i][n], key[i][j]] = [key[i][j], key[i][n]];
+    }
+
+    //returning the array as their corresponding text option
+
+    for (let m = 0; m < key[i].length; m++) {
+      key[i][m] = array[key[i][m]]; 
+    }
+  }
+  return key; 
 }
 
-showOptions(pokeImg, pokeAnswer);
+// old show options function (not being called; here for reference purposes)
+function showOptions(array, key) {
+
+  for (let i = 0; i < array.length; i++) {
+    $("#pokemonImg").attr("src", "assets/images/" + String(i) + ".png");
+    var radioDiv = $("<div>")
+    radioDiv.addClass("form-check form-check-inline");
+    $("#form").empty();
+
+    for (let j = 0; j < 4; j++) {
+
+      var radioInput = $("<input>");
+      radioInput.attr({
+
+        class: "form-check-input",
+        type: "radio",
+        name: array[i],
+        value: array[key[i][j]]
+
+      })
+
+      var radioLabel = $("<label>");
+      radioLabel
+        .addClass("form-check-label")
+        .text(array[key[i][j]]);
+      radioDiv.append(radioInput, radioLabel);
+      $("#form").append(radioDiv);
+
+      //testing to see if the text and images are actually being iterated
+      console.log(array[key[i][j]]);
+    }
+  }
+}
+
+randomOptions(pokeArray, pokeAnswer);
+
+// console.log(pokeAnswer);
+
+// });
