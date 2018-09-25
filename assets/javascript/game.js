b6e6e422c180193da2f20faffe177b1cd69ed847
baguinaga <42448094+Baguinaga@users.noreply.github.com>
2018-09-24 22:13:32 -0400

    deleted .DS_Store and excess comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
 var pokeImg = ["Pikachu", "Charmander", "Bulabasaur", "Squirtle", "Snorlax", "Gyarydos", "Scyther", "Mewtwo", "Gengar", "Kabutops", "Dragonair", "Ninetails", "Slowpoke", "Meowth", "Lapras"];
+
 var pokeAnswer = [];
-var intervalId;
 var timerCount = 0;
 var timerActive = false;
+var counter = 0;
 
 function randomNumExclude(array, arr) {
   numberArray = [arr];
@@ -20,50 +21,40 @@ function randomNumExclude(array, arr) {
 for (var i = 0; i < pokeImg.length; i++) {
   pokeAnswer.push(randomNumExclude(pokeImg, i));
 }
+console.log(pokeAnswer);
 
-console.log(pokeImg[pokeAnswer[0][0]])
-console.log(pokeImg[pokeAnswer[0][1]])
-console.log(pokeImg[pokeAnswer[0][2]])
-console.log(pokeImg[pokeAnswer[0][3]])
 
 function showOptions(array, key) {
 
-  for (var i = 0; i < array.length; i++) {
-    console.log("this is being iterated")
-    $("#pokemonImg").attr("src", "assets/images/" + String(i) + ".png");
-    console.log("assets/images/" + String(i) + ".png");
-    var radioDiv = $("<div>");
-    radioDiv.addClass("form-check form-check-inline");
-    $("#form").empty();
-    for (var j = 0; j < 4; j++) {
-      var radioInput = $("<input>");
-      radioInput.attr({
-        class: "form-check-input",
-        type: "radio",
-        name: array[i],
-        value: array[key[i][j]]
-      })
-      var radioLabel = $("<label>");
-      radioLabel
-        .addClass("form-check-label")
-        .text(array[key[i][j]]);
-      radioDiv.append(radioInput, radioLabel);
-      $("#form").append(radioDiv);
+    for (let i = 0; i < array.length; i++) {
+      $("#pokemonImg").attr("src", "assets/images/" + String(i) + ".png");
+      var radioDiv = $("<div>")
+      radioDiv.addClass("form-check form-check-inline");
+      $("#form").empty();
+
+      for (let j = 0; j < 4; j++) {
+
+        var radioInput = $("<input>");
+        radioInput.attr({
+
+          class: "form-check-input",
+          type: "radio",
+          name: array[i],
+          value: array[key[i][j]]
+
+        })
+
+        var radioLabel = $("<label>");
+        radioLabel
+          .addClass("form-check-label")
+          .text(array[key[i][j]]);
+        radioDiv.append(radioInput, radioLabel);
+        $("#form").append(radioDiv);
+
+        //testing to see if the text and images are actually being iterated
+        console.log(array[key[i][j]]);
+      }
     }
-  }
 }
-
-function checkInput() {
-  timerActive = true;
-  $(document).on("change", ".form-check-input", function () {
-    // get value and question index from input
-    var questionAnsweredIndex = $(this).attr("name");
-    var answerPicked = $(this).val();
-
-    console.log(questionAnsweredIndex);
-    console.log(answerPicked);
-
-  })
-};
 
 showOptions(pokeImg, pokeAnswer);
